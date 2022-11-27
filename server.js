00c21125b15adc1fd885db793e00423df82e2f38
unknown <asmatol@yandex.ru>
2022-11-27 16:02:03 +0300

    lesson-2

--- server.js
+++ server.js
@@ -1,42 +1,39 @@
+const EventEmitter = require('events'), emitter = new EventEmitter();
+require('moment-precise-range-plugin');
+const moment = require('moment')
 
-const colors = require("colors/safe");
+const [userDate] = process.argv.slice(2);
+const format = 'YYYY-MM-DD HH';
 
-let [firstPrime, secondPrime] = process.argv.slice(2)
+const getDateFromDateString = dateString => {
+    const [hour, day, month, year] = dateString.split('-')
+    return new Date(Date.UTC(year, month - 1, day, hour))
+}
 
-function simpleNum(num1 , num2 ) {
-    let count = 0
+const dateInFuture = getDateFromDateString(userDate);
 
-    const colorList = [colors.green, colors.yellow, colors.red]
+const showRemainingTime = dateInFuture => {
+    const dateNow = new Date();
 
-    if (!num1 || !num2) {
-        console.log(colors.red('Ошибка! Введите два простых числа'))
-
+    if (dateNow >= dateInFuture) {
+        emitter.emit('timerEnd');
     } else {
-        if (num1 < 2) {
-            num1 = 2
-        }
-
-        for (let i = num1; i <= num2; i++) {
-            let isPrime = true
-
-            for (let j = 2; j < i; j++) {
-                if (i % j === 0) {
-                    isPrime = false
-                }
-            }
-
-            if (isPrime) {
-                console.log(colorList[count % 3](i))
-                count++
-            }
-        }
-
-        if (!count) {
-            console.log(colors.red('Не найдено простых чисел в диапазоне'))
-            process.exit ()
-        }
+        const currentDateFormatted = moment(dateNow, format);
+        const futureDateFormatted = moment(dateInFuture, format);
+        console.log(moment.preciseDiff(currentDateFormatted, futureDateFormatted));
     }
 }
 
-simpleNum(Number(firstPrime), Number(secondPrime))
+const timerId = setInterval(() => {
+    emitter.emit('timerTick', dateInFuture);
+}, 500);
 
+const showTimerDone = timerId => {
+    clearInterval(timerId);
+    console.log('STOP');
+}
+
+emitter.on('timerTick', showRemainingTime)
+emitter.on('timerEnd', () => {
+    showTimerDone(timerId);
+})
